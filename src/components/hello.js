975a8e0d2fe904026a49e1bc46cfe2737e9ceca4
zubairahsan <35162568+zubairahsan@users.noreply.github.com>
2020-06-09 18:48:06 +0500

    error removed

--- src/components/hello.js
+++ src/components/hello.js
@@ -3,7 +3,7 @@ import React from "react";
 const Hello = ({ hello }) => {
   return (
     <div>
-      <h1>{hello}</h1>
+      <h1>{hello} Welcom to my surge Host New File</h1>
     </div>
   );
 };
